2fa45227eb54142fe8ec2194d5bc0f3504456936
Matt Scott <matt.scott449@gmail.com>
2022-04-05 11:50:00 +1000

    Working through components, mainly with the cards & CSS stylings.

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1, 
+        rating: 10,
+        text: "Lorem Ipsum"
+    },
+    {
+        id: 2,
+        rating: 6,
+        text: "Testing testing, 123."
+    },
+    {
+        id: 3,
+        rating: 9,
+        text: "My brother in christ, we are seeing how this goes."
+    }
+]
+
+export default FeedbackData;
